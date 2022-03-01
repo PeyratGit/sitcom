f7a84eda34a46af459ec5b20540749d92b0debc1
Canelle Deguitre <cdeguitre@outlook.fr>
2022-03-01 14:09:58 +0100

    check signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   resources :tv_shows, only: [:index, :show, :my] do
     resources :feedbacks, only: [:create, :update]
