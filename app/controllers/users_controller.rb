12694a09f91971dccddec78dbaef54fbd90b57bb
Julie Reynal <rjulie.pro@gmail.com>
2022-03-02 17:05:39 +0100

    user page activity feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @following = Follow.where(follower: @user)
     @followers = Follow.where(following: @user)
+    @feedbacks = @user.feedbacks
   end
 end
