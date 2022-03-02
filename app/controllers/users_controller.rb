387e00a2484f4e0c4e39c60cd8c4df70d1f18057
Julie Reynal <rjulie.pro@gmail.com>
2022-03-02 16:18:58 +0100

    user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @following = Follow.where(follower: @user)
+    @followers = Follow.where(following: @user)
+  end
 end
