5b944d6d6a15d7feae79730a77747296f1df4621
Clement Peyrat <aiglefeu31@gmail.com>
2022-03-04 14:27:31 +0100

    Added another stimulus controller for ajax on votes scores

---
+++ app/javascript/controllers/votes_scores_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["likescore", "dislikescore", "superlikescore"];
+
+  connect() {
+    if (this.targets) {
+      console.log(this.likescoreTarget)
+      console.log(this.dislikescoreTarget)
+      console.log(this.superlikescoreTarget)
+    }
+  }
+}
