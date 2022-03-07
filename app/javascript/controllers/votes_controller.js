a057af3b37df955e7b90c0844b8e8fe80f818bb9
Clement Peyrat <aiglefeu31@gmail.com>
2022-03-07 15:25:40 +0100

    Finished buttons and followers seen

--- app/javascript/controllers/votes_controller.js
+++ app/javascript/controllers/votes_controller.js
@@ -4,12 +4,6 @@ export default class extends Controller {
   static targets = ["likebtn", "dislikebtn", "superlikebtn", "scores"];
 
   connect() {
-    if (this.targets) {
-      console.log(this.likebtnTarget)
-      console.log(this.dislikebtnTarget)
-      console.log(this.superlikebtnTarget)
-      console.log(this.scoresTarget);
-    }
   }
   changescores(event) {
     this.scoresTarget.innerHTML = event.detail[0].response
